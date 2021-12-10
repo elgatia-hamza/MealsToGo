14a7e2db355ff5858fbbf81091ed046c1755321f
Gatia <elgatia.hamza.ai@gmail.com>
2021-12-10 21:36:29 +0100

    add this rating and exp-google-fonts

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
+// import {
+//   useFonts as usePoppins,
+//   Poppins_400Regular,
+// } from "@expo-google-fonts/poppins";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
+  // let [poppinsLoaded] = usePoppins({
+  //   Poppins_400Regular,
+  // });
+
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,12 +1,38 @@
 import React from "react";
-import  styled from "styled-components";
-import { Text, StyleSheet } from "react-native";
-import { Card, Paragraph} from "react-native-paper";
+import styled from "styled-components";
+import { Text, View } from "react-native";
+import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
 
-const Title = styled.Text`
-  color: red;
-  font-weight: bold;
-  font-size: 24px;
+import star from "../../../../assets/star";
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled(View)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Row = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -21,19 +47,20 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
-
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  console.log(ratingArray);
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Card.Content>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
         <Title>{name}</Title>
-        <Paragraph>{address}</Paragraph>
-      </Card.Content>
-    </Card>
+        <Row>
+          {ratingArray.map(() => {
+            <SvgXml xml={star} width={20} height={20} />;
+          })}
+        </Row>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
